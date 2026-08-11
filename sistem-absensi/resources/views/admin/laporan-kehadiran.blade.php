468cafbd44b7872a51988a365bffe26997fcb7f5
Azkha Nazzala <azkhanazzala@gmail.com>
2026-08-11 15:05:49 +0700

    style(report): perbaiki background halaman laporan kehadiran agar seamless

--- sistem-absensi/resources/views/admin/laporan-kehadiran.blade.php
+++ sistem-absensi/resources/views/admin/laporan-kehadiran.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Laporan Kehadiran')
 
 @section('content')
-<div x-data="{ filterOpen: false, exportModalOpen: false, exportFormat: 'excel' }" class="min-h-screen bg-slate-50 p-6">
+<div x-data="{ filterOpen: false, exportModalOpen: false, exportFormat: 'excel' }">
 
     <div class="mb-6">
         <h1 class="text-2xl font-semibold text-gray-900">Laporan Kehadiran</h1>
@@ -32,7 +32,7 @@
                 </div>
 
                 <div class="flex items-center">
-                    <div class="relative">
+                    <div class="relative" @click.outside="filterOpen = false">
                         <button
                             type="button"
                             @click="filterOpen = !filterOpen"
