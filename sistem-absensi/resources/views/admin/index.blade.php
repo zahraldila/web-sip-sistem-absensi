ed8a58ff71da75a8fe98882ba1c70517dd7d6ac8
Azkha Nazzala <azkhanazzala@gmail.com>
2026-08-12 13:48:39 +0700

    style(dashboard): ganti ikon SVG summary card dengan FontAwesome

--- sistem-absensi/resources/views/admin/index.blade.php
+++ sistem-absensi/resources/views/admin/index.blade.php
@@ -81,9 +81,7 @@
                     Total Pegawai
                 </p>
                 <div class="text-blue-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5V4H2v16h5"/>
-                    </svg>
+                    <i class="fa-solid fa-users fa-lg"></i>
                 </div>
             </div>
 
@@ -106,9 +104,7 @@
                     Hadir Hari Ini
                 </p>
                 <div class="text-green-600">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/>
-                    </svg>
+                    <i class="fa-solid fa-circle-check fa-lg"></i>
                 </div>
             </div>
 
@@ -131,9 +127,7 @@
                     WFO
                 </p>
                 <div class="text-slate-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
-                    </svg>
+                    <i class="fa-solid fa-building fa-lg"></i>
                 </div>
             </div>
 
@@ -156,9 +150,7 @@
                     WFH / WFC
                 </p>
                 <div class="text-orange-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12h18M3 12l9-9 9 9M4 10v10a2 2 0 002 2h3a2 2 0 002-2v-4h2v4a2 2 0 002 2h3a2 2 0 002-2V10"/>
-                    </svg>
+                    <i class="fa-solid fa-house-laptop fa-lg"></i>
                 </div>
             </div>
 
