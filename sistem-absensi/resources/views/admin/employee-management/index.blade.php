acc61c0797dcb075d4d1739891c7b25e73f630dc
rahmaattayat <rahmaattaya015@gmail.com>
2026-08-14 11:52:41 +0700

    fix: perbaiki posisi foto profil detail pegawai

--- sistem-absensi/resources/views/admin/employee-management/index.blade.php
+++ sistem-absensi/resources/views/admin/employee-management/index.blade.php
@@ -304,8 +304,8 @@
                 </div>
             </div>
             <div class="px-6 py-6">
-                <div class="grid gap-6 lg:grid-cols-[220px_1fr]">
-                    <div class="rounded-3xl border border-slate-200 bg-slate-50 p-6 text-center">
+                <div class="grid gap-6 lg:grid-cols-[220px_1fr] items-center">
+                    <div class="flex flex-col items-center justify-center text-center">
                         <template x-if="detailData.foto_profile">
                             <img :src="detailData.foto_profile" alt="Foto Pegawai" class="mx-auto h-28 w-28 rounded-full object-cover" />
                         </template>
